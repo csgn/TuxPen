82211517bef1d75c3e07e6d319cd43bc44f82a6b
csgn <dev.csgn@gmail.com>
2021-12-30 17:49:18 +0300

    Add ui

--- tuxpen/tuxpen.py
+++ tuxpen/tuxpen.py
@@ -1,25 +1,56 @@
+import os
 import sys
 import copy
+import pickle
+import shutil
 
 from PyQt5 import QtGui
 
-from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QPoint, Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QColorDialog, QLabel, QMainWindow, QPushButton, QWidget
 
-
-class TxCore:
-    def __init__(self, *args, **kwargs) -> None:
-        self.__txapp = TxApplication(*args, *kwargs)
-
-    def exec(self):
-        sys.exit(self.__txapp.exec_())
+from ui import Ui_Tuxpen
 
 
-class TxApplication(QApplication):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+class TxBuffer:
+    @staticmethod
+    def dump(session, buffer):
+        if len(buffer) <= 0:
+            return
 
-        self.__txwindow = TxWindow()
+        file = f"txpen{session}.pkl"
+        with open(file, "wb+") as f:
+            pickle.dump(buffer, f, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def load(session):
+        file = f"txpen{session}.pkl"
+
+        if os.path.exists(file):
+            with open(file, "rb") as f:
+                return pickle.load(f)
+
+        return []
+
+    @staticmethod
+    def clear(session):
+        file = f"txpen{session}.pkl"
+
+        if os.path.exists(file):
+            os.remove(file)
+
+
+class TxPoint:
+    def __init__(self, x: QPoint, y: QPoint):
+        self.x = x
+        self.y = y
+
+
+class TxPenAttr:
+    def __init__(self, c, t, s):
+        self.c = c
+        self.t = t
+        self.s = s
 
 
 class TxBoard(QWidget):
@@ -33,15 +64,16 @@ class TxBoard(QWidget):
         self.lastMousePosition = None
         self.currentMousePosition = None
 
-        self.buffer = []
+        self.tempBuffer = []
         self.undoBuffer = []
         self.redoBuffer = []
+
+        self.penAttr = TxPenAttr(Qt.red, 3, Qt.SolidLine)
 
         self.initUI()
 
     def initUI(self) -> None:
         self.setWindowTitle(f"Board {self.session}")
-
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setWindowState(Qt.WindowMaximized)
@@ -50,9 +82,12 @@ class TxBoard(QWidget):
         super().setGeometry(self.lastWindowPosition)
 
     def show(self) -> None:
+        self.undoBuffer = TxBuffer.load(self.session)
         super().show()
 
     def hide(self) -> None:
+        TxBuffer.dump(self.session, self.undoBuffer)
+        self.undoBuffer = []
         super().hide()
 
     def undo(self) -> None:
@@ -65,49 +100,63 @@ class TxBoard(QWidget):
             self.undoBuffer.append(self.redoBuffer.pop())
             self.update()
 
-    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
-        if not self.lastMousePosition and not self.currentMousePosition:
-            return
+    def clear(self) -> None:
+        self.tempBuffer = []
+        self.undoBuffer = []
+        self.redoBuffer = []
 
+        TxBuffer.clear(self.session)
+
+        self.update()
+
+    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
         painter = QtGui.QPainter(self)
-        painter.setPen(QtGui.QPen(Qt.red, 3, Qt.SolidLine))
 
         if self.isDrawing:
-            self.buffer.append(
-                (self.lastMousePosition, self.currentMousePosition))
+            pen = QtGui.QPen(self.penAttr.c, self.penAttr.t, self.penAttr.s)
 
+            txpoint = TxPoint(self.lastMousePosition,
+                              self.currentMousePosition)
+
+            self.tempBuffer.append([self.penAttr, txpoint])
             self.lastMousePosition = self.currentMousePosition
 
-            for pos in self.buffer:
-                painter.drawLine(*pos)
+            painter.setPen(pen)
+            for b in self.tempBuffer:
+                point = b[1]
+                painter.drawLine(point.x, point.y)
 
-        for ub in self.undoBuffer:
-            for p in ub:
-                painter.drawLine(*p)
+        for b in self.undoBuffer:
+            for p in b:
+                buffer_txpen = p[0]
+                buffer_point = p[1]
+                buffer_pen = QtGui.QPen(
+                    buffer_txpen.c, buffer_txpen.t, buffer_txpen.s)
+
+                painter.setPen(buffer_pen)
+                painter.drawLine(buffer_point.x, buffer_point.y)
 
     def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
-        print(f"(Board {self.session} - Mouse Clicked)")
+        #print(f"(Board {self.session} - Mouse Clicked)")
 
         self.currentMousePosition = a0.pos()
         self.isDrawing = True
 
-    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
-        print(f"(Board {self.session} - Mouse Released)")
-        self.isDrawing = False
-
-        if len(self.buffer) > 0:
-            self.undoBuffer.append(copy.deepcopy(self.buffer))
-            self.buffer = []
-            self.update()
-
     def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
-        print(
-            f"(Board {self.session} - Mouse Position - ({a0.x()}, {a0.y()})")
+        # print(
+        # f"(Board {self.session} - Mouse Position - ({a0.x()}, {a0.y()})")
 
         self.lastMousePosition = self.currentMousePosition
         self.currentMousePosition = a0.pos()
 
         self.update()
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        #print(f"(Board {self.session} - Mouse Released)")
+        self.isDrawing = False
+
+        self.undoBuffer.append(copy.deepcopy(self.tempBuffer))
+        self.tempBuffer.clear()
 
 
 class TxWindow(QMainWindow):
@@ -117,46 +166,42 @@ class TxWindow(QMainWindow):
         self.txboards_limit = 4
         self.txboards = self.txboards_limit*[None]
         self.currentBoard = None
+        self.offset = None
 
-        self.initUI()
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setFixedSize(132, 430)
+
+        self.ui = Ui_Tuxpen()
+        self.ui.setupUi(self)
+
+        self.initConnections()
+
         self.show()
 
-    def initUI(self):
-        self.setWindowTitle("Tuxpen")
-        self.setGeometry(100, 100, 150, 480)
+    def initConnections(self):
+        self.ui.button_pen.clicked.connect(lambda: self.on_openBoard(0))
+        self.ui.button_eraser.clicked.connect(self.on_eraserButton)
+        self.ui.button_clear.clicked.connect(self.on_clearButton)
+        self.ui.button_undo.clicked.connect(self.on_undoButton)
+        self.ui.button_redo.clicked.connect(self.on_redoButton)
+        self.ui.button_exit.clicked.connect(self.close)
 
-        self.undoButton = QPushButton("Undo", self)
-        self.undoButton.clicked.connect(self.on_UndoButton)
-        self.undoButton.move(0, 10)
+    def on_eraserButton(self):
+        pass
 
-        self.redoButton = QPushButton("Redo", self)
-        self.redoButton.clicked.connect(self.on_RedoButton)
-        self.redoButton.move(0, 50)
+    def on_changeBoard(self, session):
+        pass
 
-        self.boardButton0 = QPushButton(f"{0}", self)
-        self.boardButton0.clicked.connect(lambda: self.on_openBoard(0))
-        self.boardButton0.move(0, 100)
-
-        self.boardButton1 = QPushButton(f"{1}", self)
-        self.boardButton1.clicked.connect(lambda: self.on_openBoard(1))
-        self.boardButton1.move(0, 150)
-
-        self.boardButton2 = QPushButton(f"{2}", self)
-        self.boardButton2.clicked.connect(lambda: self.on_openBoard(2))
-        self.boardButton2.move(0, 200)
-
-        self.boardButton3 = QPushButton(f"{3}", self)
-        self.boardButton3.clicked.connect(lambda: self.on_openBoard(3))
-        self.boardButton3.move(0, 250)
-
-        self.normalMode = QPushButton("Normal Mode", self)
-        self.normalMode.clicked.connect(self.on_normalMode)
-        self.normalMode.move(0, 300)
-
-    def on_normalMode(self):
+    def on_clearButton(self):
         if self.currentBoard:
-            self.currentBoard.hide()
-            self.currentBoard = None
+            self.currentBoard.clear()
+
+    def on_pickColor(self):
+        colorDialog = QColorDialog().getColor()
+
+        if colorDialog and self.currentBoard:
+            self.currentBoard.penAttr.c = colorDialog
 
     def on_openBoard(self, session):
         if len(self.txboards) <= 0:
@@ -169,33 +214,48 @@ class TxWindow(QMainWindow):
             self.txboards[session] = TxBoard(session)
             self.currentBoard = self.txboards[session]
 
-        if self.currentBoard and session == self.currentBoard.session:
-            if not self.currentBoard.isVisible():
-                self.currentBoard.show()
+        if self.currentBoard:
+            if session == self.currentBoard.session:
+                if not self.currentBoard.isVisible():
+                    self.currentBoard.show()
+                else:
+                    self.currentBoard.hide()
             else:
                 self.currentBoard.hide()
-        else:
-            self.currentBoard.hide()
-            self.currentBoard = self.txboards[session]
-            self.currentBoard.show()
+                self.currentBoard = self.txboards[session]
+                self.currentBoard.show()
 
-    def on_UndoButton(self):
+    def on_undoButton(self):
         if self.currentBoard:
             self.currentBoard.undo()
 
-    def on_RedoButton(self):
+    def on_redoButton(self):
         if self.currentBoard:
             self.currentBoard.redo()
 
-    def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
-        print(f"(Main Mouse Position - ({a0.x()}, {a0.y()}))")
-
-    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+    def close(self) -> None:
         del self.currentBoard
         del self.txboards
-        return super().closeEvent(a0)
+        return super().close()
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
+        if e.button() == Qt.LeftButton:
+            self.offset = e.pos()
+        else:
+            super().mousePressEvent(e)
+
+    def mouseMoveEvent(self, e: QtGui.QMouseEvent) -> None:
+        if self.offset is not None and e.buttons() == Qt.LeftButton:
+            self.move(self.pos() + e.pos() - self.offset)
+        else:
+            super().mouseMoveEvent(e)
+
+    def mouseReleaseEvent(self, e: QtGui.QMouseEvent) -> None:
+        self.offset = None
+        super().mouseReleaseEvent(e)
 
 
 if __name__ == '__main__':
-    txcore = TxCore(sys.argv)
-    txcore.exec()
+    txapp = QApplication(sys.argv)
+    txwindow = TxWindow()
+    txapp.exec_()
